36525bf61f92040110c3796a8da74e5c4babdbf2
yan <yan428@mail.ru>
2024-04-14 22:56:56 +0300

    4.0

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-function App() {
+import MySelect from "./components/UI/select/MySelect";
 
+function App() {
     const [posts, setPosts] = useState([
-        {id:1, title:"Javascript", body:"Description"},
-        {id:2, title:"Javascript 2", body:"Description"},
-        {id:3, title:"Javascript 3", body:"Description"},
+        {id: 1, title: "аа", body: "бб"},
+        {id: 2, title: "гг 2", body: "аа"},
+        {id: 3, title: "вв 3", body: "яя"},
     ])
+    const [selectedSort, setSelectedSort] = useState("")
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -21,12 +23,36 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
 
-
-        return (
+    return (
         <div className={"App"}>
             <PostForm create={createPost}/>
-            <PostList remove={removePost} posts={posts} title={"Посты про JS"}/>
+            <hr style={{margin: "15px 0"}}/>
+            <div>
+                <MyInput
+                    // placeholder={}
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue={"Сортировка"}
+                    options={[
+                    {value: "title", name: "По названию"},
+                    {value: "body", name: "По описанию"},
+                ]}
+                />
+            </div>
+            {posts.length ?
+                <PostList remove={removePost} posts={posts} title={"Посты про JS"}/>
+                :
+                <h1 style={{textAlign: "center"}}>
+                    Посты не найдены!
+                </h1>
+            }
         </div>
     );
 }
