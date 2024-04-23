a01a87a92b68c10de607d654288434f4693bd7e7
yan <yan428@mail.ru>
2024-04-23 19:05:05 +0300

    12

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 
-import {routes} from "../router";
+import {privateRoutes, publicRoutes} from "../router";
 
 const AppRouter = () => {
     return (
         <Routes>
-            {routes.map(route =>
+            {privateRoutes.map(route =>
                     <Route
                         key={route.path}
                         element={<route.component />}
                         path={route.path}
                     />
                 )}
+            {publicRoutes.map(route =>
+                <Route
+                    key={route.path}
+                    element={<route.component />}
+                    path={route.path}
+                />
+            )}
             <Route path="*" element={<Navigate to="/posts" replace />} />
         </Routes>
     );
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,15 @@ import Posts from "../pages/Posts";
 import About from "../pages/About";
 import PostIdPage from "../pages/PostIdPage";
 import Error from "../pages/Error";
+import Login from "../pages/Login";
 
-export const routes = [
+export const privateRoutes = [
     {path: '/about', component: About},
     {path: '/posts', component: Posts},
     {path: '/posts/:id', component: PostIdPage},
     {path: '/error', component: Error},
+]
+
+export const publicRoutes = [
+    {path: '/login', component: Login}
 ]
